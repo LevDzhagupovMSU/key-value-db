27dcae6ed92c798cd5f4981dcec3694e1c234f94
LevDzhagupovMSU <sabzerogood338@gmail.com>
2026-04-19 18:41:00 +0300

    Update command_manager.hpp

--- include/command_manager.hpp
+++ include/command_manager.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
-
 #include "KVStorage.hpp"
 #include "client_manager_interface.hpp"
+#include "parser.hpp"
 
 class Command_manager{
     db_KV_storage& storage_;
     ClientManagerImpl& c_manager_;
+    CommandParser c_parser_{};
     mutable std::shared_mutex mtx_;
 public:
-    Command_manager(db_KV_storage& storage, ClientManagerImpl& c_manager) : storage_(storage) , c_manager_(c_manager) {}
+    Command_manager(db_KV_storage& storage, ClientManagerImpl& c_manager);
 
     std::string instruction(const std::string& message, const ClientInfo& client) const;
 private:
     bool is_admin_command(const std::string& cmd) const;
-
-    std::tuple<std::string, int, std::string> parser_cmd(const std::string& message) const;
 
     bool is_admin(const std::string& access) const;
 
